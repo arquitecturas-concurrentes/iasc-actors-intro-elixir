8586a4900cbb81c908ea2b5a45f7fd56784fe7a7
agustindavidnievas <agustindavidnievas90@gmail.com>
2020-05-18 01:46:43 -0300

    Refactor del GenServer

--- elixir/intro_actors/lib/genserver_example.ex
+++ elixir/intro_actors/lib/genserver_example.ex
@@ -1,9 +1,12 @@
 defmodule Post do
   use GenServer
   #Implementación con actores y Genserver...
+  
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
 
-  def init(_args) do
-    cantidad_inicial_likes = 0
+  def init(cantidad_inicial_likes) do
     {:ok, cantidad_inicial_likes}
   end
 
@@ -11,8 +14,14 @@ defmodule Post do
     nuevo_estado = cantidad_likes + 1
     {:noreply, nuevo_estado}
   end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end
 end
 
-#{:ok, pid_del_actor} = Post.start_link
+#{:ok, pid_del_actor} = Post.start_link(0)
 #post = pid_del_actor
-#for _ <- 1..1000, do: GenServer.cast(post, :like)
+#GenServer.call(Post, :get)
+#GenServer.cast(Post, :like)
+#for _ <- 1..1000, do: GenServer.cast(Post, :like)
