f019ba127cf6565bfd4084f02f770451fa635a5e
ernesto <ernesto@aldebaran>
2025-06-12 20:24:38 -0300

    update to 1.18

--- intro_actors/mix.exs
+++ intro_actors/mix.exs
@@ -4,7 +4,7 @@ defmodule Actors.Mixfile do
   def project do
     [app: :intro_actors,
      version: "0.0.3",
-     elixir: "~> 1.15",
+     elixir: "~> 1.18",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
