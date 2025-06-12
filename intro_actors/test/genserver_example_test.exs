49c80f1220fec0549373f82e0dfb890c76d12479
ernesto <bossi.ernestog@gmail.com>
2025-06-12 20:48:29 -0300

    update tests

--- intro_actors/test/genserver_example_test.exs
+++ intro_actors/test/genserver_example_test.exs
@@ -6,16 +6,16 @@ defmodule PostTest do
     {:ok,server: server_pid}
   end
 
-  test "post like", %{server: pid} do
-    assert :ok == GenServer.cast(Post, {:like, self})
+  test "post like", %{server: _pid} do
+    assert :ok == GenServer.cast(Post, {:like, self()})
   end
 
-  test "get total posts", %{server: pid} do
+  test "get total posts", %{server: _pid} do
     assert 0 == GenServer.call(Post, :get)
   end
 
-  test "get correct total posts", %{server: pid} do
-    for _ <- 1..1000, do: GenServer.cast(Post, {:like, self})
+  test "get correct total posts", %{server: _pid} do
+    for _ <- 1..1000, do: GenServer.cast(Post, {:like, self()})
     assert 1000 == GenServer.call(Post, :get)
   end
 end
