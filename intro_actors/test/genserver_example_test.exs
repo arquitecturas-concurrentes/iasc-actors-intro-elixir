dc6ee3cf221ad736d7744beed0154b632c91dced
ernesto <bossi.ernestog@gmail.com>
2025-06-12 21:05:53 -0300

    update tests

--- intro_actors/test/genserver_example_test.exs
+++ intro_actors/test/genserver_example_test.exs
@@ -2,20 +2,20 @@ defmodule PostTest do
   use ExUnit.Case
 
   setup do
-    {:ok,server_pid} = Post.start_link(0)
-    {:ok,server: server_pid}
+    {:ok, server_pid} = Post.start_link(0, :testPost)
+    {:ok, server: server_pid}
   end
 
-  test "post like", %{server: _pid} do
-    assert :ok == GenServer.cast(Post, {:like, self()})
+  test "post like", %{server: pid} do
+    assert :ok == Post.like(pid, self())
   end
 
-  test "get total posts", %{server: _pid} do
-    assert 0 == GenServer.call(Post, :get)
+  test "get total posts", %{server: pid} do
+    assert 0 == Post.get_likes(pid)
   end
 
-  test "get correct total posts", %{server: _pid} do
-    for _ <- 1..1000, do: GenServer.cast(Post, {:like, self()})
-    assert 1000 == GenServer.call(Post, :get)
+  test "get correct total posts", %{server: pid} do
+    for _ <- 1..1000, do: Post.like(pid, self())
+    assert 1000 == Post.get_likes(pid)
   end
 end
